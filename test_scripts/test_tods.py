5f702b277c5d06998e96682845ad36f57bcdb5a3
Ioana-Roxana <67548504+ioana-roxana-b@users.noreply.github.com>
2024-11-04 19:50:43 +0200

    ts

--- test_scripts/test_tods.py
+++ test_scripts/test_tods.py
@@ -4,25 +4,36 @@ from time_series import classification_with_tods
 
 
 def tods_test():
-    data_path = 'Outputs/Dos_Tol/chapter_feature.csv'  # Path to your data file
-    classifier_config_path = 'time_series/tods_classifiers_config.json'  # Path to your configuration file
+    """
+    Executes time-series classification on a specified dataset using configurations
+    defined in an external JSON file. Each classifier's settings, preprocessing steps,
+    and feature extraction methods are specified for streamlined testing and evaluation.
+    Params:
+        None
+    Returns:
+        None: Outputs classification results and saves them to files or logs them as needed.
+    """
 
-    # Load the dataset
+    #The path to the data file and classifier configuration JSON file
+    data_path = 'Outputs/Dos_Tol/chapter_feature.csv'
+    classifier_config_path = 'time_series/tods_classifiers_config.json'
+
+    # Load the dataset into a DataFrame
     data_df = pd.read_csv(data_path)
 
-    # Load TODS classifier configurations from a JSON file
+    # Load classifier configurations from the JSON file
     with open(classifier_config_path, 'r') as f:
         classifiers_config = json.load(f)
 
-    # Iterate over each classifier defined in the configuration
+    # Iterate over each classifier specified in the configuration file
     for classifier_name, params in classifiers_config.items():
-        # Extract parameters for the classifier
+        # Extract configuration details
         classifiers_list = params.get('classifiers', [classifier_name])
         as_time_series = params.get('as_time_series', False)
         preprocessing_methods = params.get('preprocessing_methods', [])
         preprocessing_with_tods = params.get('preprocessing_with_tods', [])
 
-        # Call the classification_with_tods function with the extracted parameters
+        # Execute the classification function with the extracted parameters
         classification_with_tods.classification_with_tods(
             classifiers=classifiers_list,
             data_df=data_df,
